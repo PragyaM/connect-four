04954b0d7606c83906ac3d04fb05941e516a56be
Pragya <pragya.mohan@powershop.co.nz>
2014-12-22 16:29:38 +1300

    updated tests

--- spec/services/check_connections_spec.rb
+++ spec/services/check_connections_spec.rb
@@ -4,8 +4,12 @@ RSpec.describe CheckConnections  do
   let(:grid) do 
     [[],[],[],[],[],[],[]]
   end
+
+  fixtures :games, :turns
+  let(:game) { games(:playing_game) }
   
-  let(:last_turn) {instance_double("Turn")}
+  let(:last_turn_lane_3) { turns(:last_turn_player1_lane3) }
+  let(:last_turn_lane_0) { turns(:last_turn_player1_lane0) }
 
   describe "#call" do
     context "when there are four in a row horizontally" do
@@ -14,11 +18,11 @@ RSpec.describe CheckConnections  do
           grid[lane].push(1)
         end
 
-        expect(last_turn).to receive(:user_id).and_return 1
-        expect(last_turn).to receive(:lane_number).and_return 3
+        expect(last_turn_lane_3).to receive(:player).and_return 1
+        expect(last_turn_lane_3).to receive(:lane_number).and_return 3
       end
       
-      let(:check_connections) {CheckConnections.new(grid, last_turn)}
+      let(:check_connections) {CheckConnections.new(grid, game)}
 
       it "finds the match" do
         expect(check_connections.call).to be true
@@ -31,11 +35,12 @@ RSpec.describe CheckConnections  do
           grid[0].push(1)
         end
 
-        expect(last_turn).to receive(:user_id).and_return 1
-        expect(last_turn).to receive(:lane_number).and_return 0
+        allow(game.turns).to receive(:size).and_return 4
+        expect(last_turn_lane_0).to receive(:player).and_return 1
+        expect(last_turn_lane_0).to receive(:lane_number).and_return 0
       end
       
-      let(:check_connections) {CheckConnections.new(grid, last_turn)}
+      let(:check_connections) {CheckConnections.new(grid, game)}
 
       it "finds the match" do
         expect(check_connections.call).to be true
@@ -51,11 +56,11 @@ RSpec.describe CheckConnections  do
           grid[lane].push(1)
         end
 
-        expect(last_turn).to receive(:user_id).and_return 1
-        expect(last_turn).to receive(:lane_number).and_return 0
+        expect(last_turn_lane_0).to receive(:player).and_return 1
+        expect(last_turn_lane_0).to receive(:lane_number).and_return 0
       end
       
-      let(:check_connections) {CheckConnections.new(grid, last_turn)}
+      let(:check_connections) {CheckConnections.new(grid, game)}
 
       it "finds the match" do
         expect(check_connections.call).to be true
@@ -71,11 +76,11 @@ RSpec.describe CheckConnections  do
           grid[lane].push(1)
         end
 
-        expect(last_turn).to receive(:user_id).and_return 1
-        expect(last_turn).to receive(:lane_number).and_return 0
+        expect(last_turn_lane_0).to receive(:player).and_return 1
+        expect(last_turn_lane_0).to receive(:lane_number).and_return 0
       end
       
-      let(:check_connections) {CheckConnections.new(grid, last_turn)}
+      let(:check_connections) {CheckConnections.new(grid, game)}
 
       it "finds the match" do
         expect(check_connections.call).to be true
@@ -87,12 +92,9 @@ RSpec.describe CheckConnections  do
         grid[0].push(1)
         grid[2].push(0)
         grid[3].push(1)
-
-        expect(last_turn).to receive(:user_id).and_return 1
-        expect(last_turn).to receive(:lane_number).and_return 0
       end
       
-      let(:check_connections) {CheckConnections.new(grid, last_turn)}
+      let(:check_connections) {CheckConnections.new(grid, game)}
 
       it "finds the match" do
         expect(check_connections.call).to be false
